8e320a3d421df6fb7cd32c29f4b5a14e75763d1a
gkochera <kocherag@oregonstate.edu>
2020-07-16 13:06:48 -0400

    created query for product_list route and created handlebars for product_list.hbs

--- app.js
+++ app.js
@@ -94,7 +94,13 @@ app.get('/', function(req, res) {
 
 // Product List Route
 app.get('/product_list', function(req, res) {
-    res.render('product_list')
+    connection.query('SELECT * FROM `products`', function(error, results, fields){
+        if (error) {
+            console.log("Error loading inventory page: " + error)
+        }
+        res.render('product_list', results)
+        console.log(results)
+    })
 })
 
 app.get('/inventory', function(req, res) {
