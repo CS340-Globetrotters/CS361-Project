c105e15aaffbd66a6bcc72d1ec306cc5ebbca522
yangellen <49252706+yangellen@users.noreply.github.com>
2020-07-21 10:57:37 -0700

    modified app.js to check low shelf notification for active products only

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ app.get('/inventory', function(req, res) {
     var inventoryQueryString =    
         `SELECT id, name, shelf_quantity, DATE_FORMAT(exp_date,'%m-%d-%Y') AS exp_date, 
         wh_quantity, shelf_quantity + wh_quantity AS total_quantity,
-        shelf_min_threshold, shelf_max_threshold FROM products;`
+        shelf_min_threshold, shelf_max_threshold, active FROM products;`
 
     // Requesting the data from the database
     connection.query(inventoryQueryString, function(error, results, fields){
@@ -115,9 +115,9 @@ app.get('/inventory', function(req, res) {
         }
 
         console.log(results)
-        // Check for items that are low on the shelf and set .shelf_low to true if they are
+        // Check for "Active" items that are low on the shelf and set .shelf_low to true if they are
         results.forEach(function(value, index) {
-            if (value.shelf_quantity < value.shelf_min_threshold) {
+            if (value.shelf_quantity < value.shelf_min_threshold && value.active != 0) {
                 value.shelf_low = true
             } else {
                 value.shelf_low = false
