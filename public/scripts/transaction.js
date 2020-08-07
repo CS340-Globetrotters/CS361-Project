bf674ad377059db6ec8fa5f72cec62dd7cb51477
George Kochera <george.kochera@gmail.com>
2020-08-07 17:56:28 -0400

    created transaction.js for AJAX and DOM manipulation and started framework for transaction handlebars template

---
+++ public/scripts/transaction.js
@@ -0,0 +1,8 @@
+/*
+transaction.js
+Author: George Kochera
+Description: Handles the AJAX calls necessary for keeping the checkout basket up to date and populating the available items for the user to select during checkout.
+*/
+
+const product_window = document.querySelector('#gt_available_products_on_shelf_window')
+const basket_window = document.querySelector('#gt_products_in_basket_window')
